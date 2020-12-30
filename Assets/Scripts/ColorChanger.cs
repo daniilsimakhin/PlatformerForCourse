774925ab50a67b46314546c52583285afa8dc22a
daniilsimakhin <52642856+daniilsimakhin@users.noreply.github.com>
2020-12-31 01:45:24 +0700

    Version 1.4

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField] private Image _background;
+
+    private Color _startColor;
+
+    public void Start()
+    {
+        _startColor = _background.color;
+    }
+
+    public void ChangeColor(float value)
+    {
+        if (value == 0)
+        {
+            _background.color = _startColor;
+        }
+        else
+        {
+            _background.color = new Color(1 - value, 1 - value, value);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _canvas;
+
+    public void ButtonPlay()
+    {
+        _canvas.blocksRaycasts = false;
+        _canvas.DOFade(0, 1);
+    }
+}
