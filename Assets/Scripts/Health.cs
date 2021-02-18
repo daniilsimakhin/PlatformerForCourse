ad8f4d8e952b54da456070fc6d85d417bcef2a7f
daniilsimakhin <52642856+daniilsimakhin@users.noreply.github.com>
2021-02-19 01:16:03 +0700

    Version 1.5

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Slider _rectSlider;
+    [SerializeField] private float _health;
+    [SerializeField] private float _hp;
+    
+    public void Healing()
+    {
+        _health = Mathf.Clamp(_health + 0.1f, 0, 1);
+        _rectSlider.DOValue(_health, 0.3f);
+    }
+
+    public void Kick()
+    {
+        _health = Mathf.Clamp(_health - 0.1f, 0, 1);
+        _rectSlider.DOValue(_health, 0.3f);
+    }
+}
